27b5e59234bb95854448cfa0b3206ff55f6ca2ab
Joao Pedro Muller <jp99muller@gmail.com>
2023-01-12 16:44:54 -0300

    Ajuste página canceled

--- pages/api/stripe.ts
+++ pages/api/stripe.ts
@@ -53,7 +53,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
             quantity: item.quantidade,
           };
         }),
-        cancel_url: `${req.headers.origin}/canceled`,
+        cancel_url: `${req.headers.origin}/`,
         success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
       });
 
